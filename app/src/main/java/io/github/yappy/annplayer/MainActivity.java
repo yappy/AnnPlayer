55d586a27658e0dc64d69136875d7457a6382a66
yappy <superunyo@yahoo.co.jp>
2019-01-03 00:04:22 +0900

    SD カードにこのアプリのデータディレクトリを作るか聞くダイアログを出す

--- app/src/main/java/io/github/yappy/annplayer/MainActivity.java
+++ app/src/main/java/io/github/yappy/annplayer/MainActivity.java
@@ -1,7 +1,12 @@
 package io.github.yappy.annplayer;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,16 +24,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initializeListArea();
-
         FloatingActionButton playButton = findViewById(R.id.fab1);
         playButton.setOnClickListener((view) -> {
-            Toast.makeText(MainActivity.this, "play " + selectedIndex, Toast.LENGTH_SHORT).show();
+            showToast("play " + view.getTag());
         });
         FloatingActionButton stopButton = findViewById(R.id.fab2);
         stopButton.setOnClickListener((view) -> {
-            Toast.makeText(MainActivity.this, "stop", Toast.LENGTH_SHORT).show();
+            showToast("stop");
         });
+
+        loadSdCard();
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadSdCard() {
+        String state = Environment.getExternalStorageState();
+        if (!Environment.MEDIA_MOUNTED.equals(state) && !Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            showToast("SD card is not found");
+            return;
+        }
+        new SdCardInitDialog().show(getSupportFragmentManager(), "SdInit");
+        initializeListArea();
     }
 
     private void initializeListArea() {
@@ -62,6 +81,27 @@ public class MainActivity extends AppCompatActivity {
         }
         if (n >= 0) {
             playListButtons[n].setBackgroundColor(Color.RED);
+        }
+    }
+
+    private static class SdCardInitDialog extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Create a directory and files for this app?")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
         }
     }
 
