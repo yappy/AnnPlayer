8db36162ebb9b39a480cbcc38087160aa9302037
yappy <superunyo@yahoo.co.jp>
2019-01-02 21:01:20 +0900

    GUI のモックアップを作成

--- app/src/main/java/io/github/yappy/annplayer/MainActivity.java
+++ app/src/main/java/io/github/yappy/annplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.yappy.annplayer;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeListArea();
+    }
+
+    private void initializeListArea() {
+        LinearLayout area = findViewById(R.id.list_area);
+        area.removeAllViews();
+        for (int i = 0; i < 100; i++) {
+            View view = getLayoutInflater().inflate(R.layout.list_button, null);
+            Button button = view.findViewById(R.id.list_button);
+            button.setText("Sound " + i);
+            area.addView(view);
+        }
     }
 }
