245c9cb06f249433de68a6133507184e0a752cd0
yappy <superunyo@yahoo.co.jp>
2019-01-03 11:56:28 +0900

    Android Studio の出す警告に対応
    - Fragment クラスは public にする
    - ラムダ式が使えるところでは使う
    - コメントを追加

--- app/src/main/java/io/github/yappy/annplayer/MainActivity.java
+++ app/src/main/java/io/github/yappy/annplayer/MainActivity.java
@@ -1,7 +1,6 @@
 package io.github.yappy.annplayer;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
@@ -19,11 +18,13 @@ public class MainActivity extends AppCompatActivity {
     private int selectedIndex = -1;
     private Button[] playListButtons;
 
+    // 初期化時に一度だけ呼ばれる
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 再生停止ボタンのクリックイベント設定
         FloatingActionButton playButton = findViewById(R.id.fab1);
         playButton.setOnClickListener((view) -> {
             showToast("play " + selectedIndex);
@@ -36,10 +37,12 @@ public class MainActivity extends AppCompatActivity {
         loadSdCard();
     }
 
+    // Toast (short) を表示する
     private void showToast(String msg) {
         Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 
+    // SD カードの内容を確認して UI に反映する
     private void loadSdCard() {
         String state = Environment.getExternalStorageState();
         if (!Environment.MEDIA_MOUNTED.equals(state) && !Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
@@ -84,21 +87,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private static class SdCardInitDialog extends DialogFragment {
+    // SD カードにディレクトリを作る確認ダイアログ
+    public static class SdCardInitDialog extends DialogFragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the Builder class for convenient dialog construction
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage("Create a directory and files for this app?")
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // FIRE ZE MISSILES!
-                        }
+                    .setPositiveButton("OK", (dialog, id) -> {
+                        // OK
                     })
-                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User cancelled the dialog
-                        }
+                    .setNegativeButton("Cancel", (dialog, id) -> {
+                        // Cancel
                     });
             // Create the AlertDialog object and return it
             return builder.create();
